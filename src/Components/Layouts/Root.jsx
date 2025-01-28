b9a863d241c9e8d03c8a84191a2b8598eec71c96
rubaiiyat <abirr1516@gmail.com>
2025-01-28 22:51:55 +0600

    add footer and change the navbar color

--- src/Components/Layouts/Root.jsx
+++ src/Components/Layouts/Root.jsx
@@ -5,12 +5,14 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar></Navbar>
       <div className="mx-5 md:mx-16 lg:mx-28">
         <Outlet></Outlet>
       </div>
-      <Footer></Footer>
+      <div className="sticky top-[100vh] bg-gray-800 text-white ">
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   );
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-200 p-3">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
