2dded637a1eeae7b40aee8eedabc740547719fdd
rubaiiyat <abirr1516@gmail.com>
2025-01-30 23:36:07 +0600

    Add about page

--- src/Components/Firebase/Firebase.js
+++ src/Components/Firebase/Firebase.js
@@ -6,6 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyAscF4gW19w1wiAYTsyJA864n6-Kjahns8",
   authDomain: "email-password-authentic-b771f.firebaseapp.com",
--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>This is about</h1>
+    <div className="mx-5 md:mx-72 text-white mt-10 text-lg">
+      Welcome to <span className="text-primary text-bold">Luxe Estate</span>,
+      where luxury meets exclusivity. We specialize in high-end real estate,
+      offering the most prestigious and sophisticated properties in prime
+      locations. Whether you’re searching for a modern penthouse, a beachfront
+      villa, or a private estate,{" "}
+      <span className="text-primary text-bold">Luxe Estate</span> ensures an
+      unparalleled buying experience tailored to your desires. Our team of
+      experienced real estate professionals is dedicated to providing
+      personalized services, ensuring that every property we offer meets the
+      highest standards of luxury, elegance, and comfort. At Luxe Estate, we
+      don’t just sell homes—we create lifestyles. Experience the art of luxury
+      living with Luxe Estate.
     </div>
   );
 };
