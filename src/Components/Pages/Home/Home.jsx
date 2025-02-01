d96fd669709e9cc5ca9ae97af3bdb7cee28c84ad
rubaiiyat <abirr1516@gmail.com>
2025-02-01 23:08:52 +0600

    add explore all things property

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -66,6 +66,53 @@ const Home = () => {
           </button>
         </Link>
       </div>
+
+      <div className="">
+        <h1 className="text-xl md:text-4xl text-white font-bold ">
+          Explore all things property
+        </h1>
+        <div className="flex gap-5 flex-wrap justify-center  mt-10">
+          <div className="w-72 md:w-96 bg-base-200 rounded-2xl py-20 p-5  md:p-20 space-y-5 mx-auto relative">
+            <h1 className="text-4xl text-white font-bold">Buy a home</h1>
+            <p>
+              Find your place with an immersive photo experience and the most
+              listings, including things you won’t find anywhere else.
+            </p>
+            <Link
+              to={"/properties"}
+              className="btn btn-outline btn-primary absolute bottom-5"
+            >
+              Browse homes
+            </Link>
+          </div>
+          <div className="w-72 md:w-96 bg-base-200 rounded-2xl py-20 p-5  md:p-20 space-y-5 mx-auto relative">
+            <h1 className="text-4xl text-white font-bold">Sell a home</h1>
+            <p>
+              No matter what path you take to sell your home, we can help you
+              navigate a successful sale.
+            </p>
+            <Link
+              to={"/properties"}
+              className="btn btn-outline btn-primary absolute bottom-5"
+            >
+              See Your Options
+            </Link>
+          </div>
+          <div className="w-72 md:w-96 bg-base-200 rounded-2xl py-20  p-5  md:p-20 space-y-5 mx-auto relative">
+            <h1 className="text-4xl text-white font-bold">Rent a home</h1>
+            <p>
+              We’re creating a seamless online experience – from shopping on the
+              largest rental network, to applying, to paying rent.
+            </p>
+            <Link
+              to={"/properties"}
+              className="btn btn-outline btn-primary absolute bottom-5"
+            >
+              Find Rentals
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
